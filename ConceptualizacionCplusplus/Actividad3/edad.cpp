5d10fe64e35c2151f62231539e016acbf154b4a5
Ing. Danny Colmenares Pérez <dannycolmenarespdv@gmail.com>
2018-08-20 02:04:59 -0400

    Add files via upload

---
+++ ConceptualizacionCplusplus/Actividad3/edad.cpp
@@ -0,0 +1,61 @@
+﻿/*
+Programa que realiza el calculo de la ubicacion de un individuo en grupos de edad
+Se tienen como datos la edad de niño
+Programa para cumplir con la actividad 3 del curso de Conceptualizacion del lenguaje C++ del SENA.
+
+Programa realizado por Ing. Danny Colmenares
+@dbcolmenaresp
+https://www.github.org/dbcolmenaresp
+*/
+
+#include <iostream>
+
+using namespace std;
+
+//Esta es la funcion principal del programa donde comienza la ejecucion del mismo
+
+int main()
+
+{
+
+//Se define una variable para almacenar el valor de la edad del niño 
+
+    int edad;
+
+//Se muestra en pantalla el mensaje del funcionamiento del programa
+
+    cout << "\nPrograma para ubicar el niño en el grupo en funcon del valor de la edad\n" << endl;
+
+
+//Se recibe el valor de la edad del niño desde el teclado
+
+    cout << "\nIngrese el valor de la edad del niño. El valor de edad correcto debe estar comprendido entre 0 y 18 años\n" << endl;
+
+    cin >> edad;
+
+    cout << "\nLa edad indicada es: " << edad << endl;
+
+
+//Se determina la pertenencia del niño a un grupo dependiendo de la edad indicada
+
+    if(edad <= 6){
+
+        cout << "\nEl niño pertenece al grupo de la primera infancia" << endl;
+
+    }else if(edad <= 12){
+
+        cout << "\nEl niño pertenece al grupo de la segunda infancia" << endl;
+
+    }else if(edad <= 18){
+
+        cout << "\nEl niño pertenece al grupo de los adolescentes" << endl;
+
+    }else
+        cout << "\nPertenece a un grupo de edades diferente" << endl;
+
+
+    system("pause");
+
+    return 0;
+
+}
---
+++ ConceptualizacionCplusplus/Actividad3/promedio.cpp
@@ -0,0 +1,61 @@
+﻿/*
+
+Este es un programa que solicita las cuatro notas y calcula el valor promedio
+Las notas entre 3.5 y 5.0 son aprobadas
+Las notas entre 3.0 y 3.5 son reprobadas pero tienen una oportunidad
+Las notas por debajo de 3.0 son no aprobadas.
+
+Programa realizado por Ing. Danny Colmenares
+@dbcolmenaresp
+https://www.github.org/dbcolmenaresp
+*/
+
+#include <iostream>
+
+using namespace std;
+
+//Inicio de la funcion principal
+int main()
+
+{
+
+    cout << "\nIngrese los cuatro valores de las notas\nRecuerde que las notas deben estar comprendidas entre 0.0 y 5.0\n" << endl;
+
+    float nota, suma, promedio;
+
+    suma = 0;
+
+    for (int i = 1; i <= 4; i++){
+
+        cin >> nota;
+
+        suma = suma + nota;
+
+    }
+
+    promedio = suma / 4;
+
+
+//Se determina el resultado de la calificacion
+
+    if(promedio <= 3.0){
+
+        cout << "\nEl estudiante no esta aprobado" << endl;
+
+    }else if(promedio > 3.0 && promedio <= 3.5){
+
+        cout << "\nEl estudiante no tiene la nota aprobada pero tiene la oportunidad de recuperar" << endl;
+
+    }else if(promedio > 3.5){
+
+        cout << "\nEl estudiante esta aprobado" << endl;
+
+    }else
+        cout << "\nRevise si los datos estan correctos" << endl;
+
+    cout << "\nDanny Colmenares" << endl;
+
+    system("pause");
+
+    return 0;
+}
