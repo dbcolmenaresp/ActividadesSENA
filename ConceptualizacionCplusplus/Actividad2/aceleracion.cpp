132c335afd0c27f6946a6bd2e5a40404ba6ef87b
Ing. Danny Colmenares Pérez <dannycolmenarespdv@gmail.com>
2018-08-20 01:41:23 -0400

    Subir codigo fuente aceleracion y tablamultiplicar

---
+++ ConceptualizacionCplusplus/Actividad2/aceleracion.cpp
@@ -0,0 +1,62 @@
+﻿/*
+Programa que realiza el calculo de la aceleracion de un vehiculo
+Se tienen como datos la velocidad inicial, la velocidad final y el tiempo de medida de la velocidad
+Programa para cumplir con la actividad 2 del curso de Conceptualizacion del lenguaje C++ del SENA.
+
+Programa realizado por Ing. Danny Colmenares
+@dbcolmenaresp
+https://www.github.org/dbcolmenaresp
+*/
+
+#include <iostream>
+
+using namespace std;
+
+//Esta es la funcion principal del programa donde comienza la ejecucion del mismo
+
+int main()
+
+{
+
+/*
+Se definen dos variables para almacenar valores de tipo real donde se guardan 
+los valores de la velocidad inicial, velocidad final y tiempo de medida de las velocidades.
+*/
+
+    double velocidad_inicial, velocidad_final, tiempo, aceleracion;
+
+//Se muestra en pantalla el mensaje indicado
+
+    cout << "\nPrograma para calcular el valor de la aceleracion de un vehiculo\n" << endl;
+
+
+//Se recibe el valor de la velocidad inicial desde el teclado
+
+    cout << "\nIngresa el valor de la velocidad inicial\n" << endl;
+
+    cin >> velocidad_inicial;
+
+//Se recibe el valor de la velocidad final desde el teclado
+
+    cout << "\nIngresa el valor de la velocidad final\n" << endl;
+
+    cin >> velocidad_final;
+
+//Se recibe el valor del tiempo de medicion desde el teclado
+
+    cout << "\nIngresa el valor del tiempo de medicion\n" << endl;
+
+    cin >> tiempo;
+
+
+    aceleracion = (velocidad_final - velocidad_inicial) / tiempo;
+
+//Se muestra en pantalla el resultado del valor de la aceleracion
+
+    cout << "\nLa aceleracion del vehiculo es: " << aceleracion << endl;
+
+    system("pause");
+
+    return 0;
+
+}
---
+++ ConceptualizacionCplusplus/Actividad2/tablamultiplicar.cpp
@@ -0,0 +1,55 @@
+﻿/*
+
+Este es un programa que solicita al usuario el ingreso por teclado de un numero y muestra 
+como resultado en pantalla la tabla de multiplicar de dicho numero ingresado.
+
+Programa realizado por Ing. Danny Colmenares
+@dbcolmenaresp
+https://www.github.org/dbcolmenaresp
+*/
+
+/*
+Directiva de preprocesador que permite usar la biblioteca de funciones 
+de entrada salida del lenguaje C++.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+//Inicio de la funcion principal
+int main()
+
+{
+
+
+    int entero;
+
+//Se muestra en pantalla el mensaje indicado
+
+    cout << "\nIndique un numero entero al que se le mostrara la tabla de multiplicar\n" << endl;
+
+//Se recibe el primer operando desde el teclado y se almacena en la variable a
+
+    cin >> entero;
+
+//Se muestra la tabla de multiplicar del numero ingresado por pantalla
+
+    cout << "\nTabla de multiplicar de %d: \n" << entero << endl;
+
+/*
+Se ejecuta un ciclo desde el numero 1 hasta el numero 10 y se multiplica sucesivamente el entero ingresado por teclado
+por cada valor del ciclo.
+*/
+
+    for (int i = 1; i <= 10; i++){
+
+        cout << i << " X " << entero << " = " << i * entero <<"\n" << endl;
+
+    }
+
+    system("pause");
+
+    return 0;
+
+}
